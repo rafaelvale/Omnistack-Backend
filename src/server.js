5d90116f7fc549ed0c33d78f1649a1f002253299
Rafael Marques Vale <rmarquev@everis.com>
2019-04-10 20:52:23 -0300

    alterando a url

--- src/server.js
+++ src/server.js
@@ -32,5 +32,5 @@ app.use("/files", express.static(path.resolve(__dirname, "..", "temp")));
 
 app.use(require("./routes"));
 
-server.listen(process.env.PORT || 3333);
+server.listen(process.env.PORT);
 
