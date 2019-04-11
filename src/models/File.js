556e7dc8c62821e9b344c8fca40df65d6cb85eac
Rafael Marques Vale <rmarquev@everis.com>
2019-04-10 21:05:11 -0300

    arrumando todas as rotas

--- src/models/File.js
+++ src/models/File.js
@@ -7,22 +7,24 @@ const File = new mongoose.Schema({
     },
     path:{
         type: String,
-        required: true,
+        required: true
     }
-}, {
+}, 
+{
     timestamps: true,
-    toObject: { virtuals: true},
-    toJSON: {virtuals: true}
-});
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true } 
+}
+);
 
 File.virtual("url").get(function() {
 
-    const url = process.env.URL //||  'http://localhost:3333'
+    const url = process.env.URL || 'http://localhost:3333'
     
     return `${url}/files/${encodeURIComponent(this.path)}`;
 });
 
-module.exports = mongoose.model('File', File);
+module.exports = mongoose.model("File", File);
 
 
 
